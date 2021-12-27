d49dc56e2aea2ab9869dfe43f5b952622e091201
RyoheiTojo <ryohei.mmeve@gmail.com>
2021-12-27 22:51:14 +0900

    power on function

--- usecase/oci.py
+++ usecase/oci.py
@@ -25,3 +25,16 @@ class OCIInteractor:
       ) for instance in response.data]
 
     return instances
+
+  def power_on_instance(self, compute_id:str) -> domain.entity.instance:
+    compute_client = oci.core.ComputeClient(config={}, signer=self.signer)
+    response = compute_client.instance_action(instance_id=compute_id, action="START")
+
+    return domain.entity.instance(
+        id                  = response.data.id,
+        availability_domain = response.data.availability_domain,
+        compartment_id      = response.data.compartment_id,
+        display_name        = response.data.display_name,
+        fault_domain        = response.data.fault_domain,
+        shape               = response.data.shape,
+      )
